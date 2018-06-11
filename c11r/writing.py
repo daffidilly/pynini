cec330602d26f056ab45cb3b547277d9d7a49372
AE <daffidilly.opensource@gmail.com>
2018-06-11 22:52:14 +0800

    rm old file
    Note that at the moment generating `release` dir from `web` (or other source dir)
    works well, and jinja generates all files ending in `.jinja2` and all other files
    are copied as-is (with stat data intact).
    
    Limitations of current in-flight work:
    
    - no concept of data files at all (so `staff.html.jinja2` never loads `staff.yml`
      or any other YAML file)
    
    - no concept of generating pages based on data.

--- c11r/writing.py
+++
@@ -1,9 +0,0 @@
-# # TODO DELETE
-# class TemplateFileWriter(object):
-#     """A writer that formats the template to a file."""
-#
-#     def __init__(self):
-#         pass
-#
-#     def write(self, template, context, out_path):
-#         template.stream(context).dump(out_path)
